0c8b0cf71cf3337c18c2ab536dd4a2358fa85c51
Samuel NABET <samuel.nabet@epita.fr>
2018-06-08 13:52:33 +0100

    Add mainview listener

--- app/src/main/java/fr/epita/theghibliapp/MainActivity.kt
+++ app/src/main/java/fr/epita/theghibliapp/MainActivity.kt
@@ -2,11 +2,19 @@ package fr.epita.theghibliapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonFilm.setOnClickListener(this@MainActivity)
+        buttonChar.setOnClickListener(this@MainActivity)
+        buttonQuiz.setOnClickListener(this@MainActivity)
     }
 }
