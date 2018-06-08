1841e0ad14b6521a94e53db34ae8a5afb5fcfad7
Samuel NABET <samuel.nabet@epita.fr>
2018-06-08 14:26:36 +0100

    Add change intent to main activity

--- app/src/main/java/fr/epita/theghibliapp/MainActivity.kt
+++ app/src/main/java/fr/epita/theghibliapp/MainActivity.kt
@@ -1,13 +1,34 @@
 package fr.epita.theghibliapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(p0: View?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onClick(v: View?) {
+        if (v!= null) {
+            when (v.id) {
+                R.id.buttonChar -> {
+                    val intent = Intent(this, CharactersList::class.java)
+                    startActivity(intent)
+
+
+                }
+                R.id.buttonFilm -> {
+                    val intent = Intent(this, CharactersList::class.java)
+                    startActivity(intent)
+
+                }
+                R.id .buttonQuiz -> {
+                    val intent = Intent(this, CharactersList::class.java)
+                    startActivity(intent)
+
+                }
+            }
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
